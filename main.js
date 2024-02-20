1d16d685bdea6c5991a2b62e2c19eff0bd1215e7
MRX <131702289+hhamidullohgit@users.noreply.github.com>
2024-02-20 14:30:00 +0500

    add file

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ let text2 = text1.lastIndexOf("salom");
 console.log(text2)
 
 let text3 = "salom men hamidulloh man man sani nma qilaman bilasamnmmi"
-let text4 = text3.substr(7, 10);
+let text4 = text3.toLowerCase();
 console.log(text4)
+
+let text5 = "hamidulloh"
+let text6 = "hikmatullayev"
+let text7 = text5.concat(" ",text6)
+let text8 = text5.indexOf('ham')
+console.log(text7)
+console.log(text8)
 
  // let text = "Please locate where 'locate' occurs!";
 // let index = text.indexOf("locate");
