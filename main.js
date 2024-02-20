5ff59858f57bd04c2a29e71c111fb472ca1e3f87
MRX <131702289+hhamidullohgit@users.noreply.github.com>
2024-02-20 10:33:46 +0500

    add file

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let name = "hamidulloh"
 let surname = "hikmatullayev"
 console.log(surname)
+
+let text1 = "salom manign ismim hamidulloh salom sening isming nima";
+let text2 = text1.lastIndexOf("salom");
+console.log(text2)
+
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
