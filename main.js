980acfc3d0a7b5ffdc2b1fd87f65d96e2e9e85cb
MRX <131702289+hhamidullohgit@users.noreply.github.com>
2024-02-20 11:47:30 +0500

    add new file

--- main.js
+++ main.js
@@ -6,5 +6,9 @@ let text1 = "salom manign ismim hamidulloh salom sening isming nima";
 let text2 = text1.lastIndexOf("salom");
 console.log(text2)
 
-// let text = "Please locate where 'locate' occurs!";
+let text3 = "salom men hamidulloh man man sani nma qilaman bilasamnmmi"
+let text4 = text3.substr(7, 10);
+console.log(text4)
+
+ // let text = "Please locate where 'locate' occurs!";
 // let index = text.indexOf("locate");
