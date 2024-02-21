055c4139d587954178d9a68992c905867052fe02
MRX <131702289+hhamidullohgit@users.noreply.github.com>
2024-02-21 19:13:01 +0500

    sasasa

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-let name = "hamidulloh"
-let surname = "hikmatullayev"
-console.log(surname)
+// let name = "hamidulloh"
+// let surname = "hikmatullayev"
+// console.log(surname)
 
-let text1 = "salom manign ismim hamidulloh salom sening isming nima";
-let text2 = text1.lastIndexOf("salom");
-console.log(text2)
+// let text1 = "salom manign ismim hamidulloh salom sening isming nima";
+// let text2 = text1.lastIndexOf("salom");
+// console.log(text2)
 
-let text3 = "salom men hamidulloh man man sani nma qilaman bilasamnmmi"
-let text4 = text3.toLowerCase();
-console.log(text4)
+// let text3 = "salom men hamidulloh man man sani nma qilaman bilasamnmmi"
+// let text4 = text3.toLowerCase();
+// console.log(text4)
 
-let text5 = "hamidulloh"
-let text6 = "hikmatullayev"
-let text7 = text5.concat(" ",text6)
-let text8 = text5.indexOf('ham')
-console.log(text7)
-console.log(text8)
+// let text5 = "hamidulloh"
+// let text6 = "hikmatullayev"
+// let text7 = text5.concat(" ",text6)
+// let text8 = text5.indexOf('ham')
+// console.log(text7)
+// console.log(text8)
 
-let text9 = "salom"
-let text10 = text9.charAt(0)
-console.log(text10)
+// let text9 = "salom"
+// let text10 = text9.charAt(0)
+// console.log(text10)
 
 
 let text11 = "hamidulloh"
