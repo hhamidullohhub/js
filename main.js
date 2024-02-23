ad2b6a8d19719ac6ca23b628380f7058a90a43d1
MRX <131702289+hhamidullohgit@users.noreply.github.com>
2024-02-23 19:33:55 +0500

    add file

--- main.js
+++ main.js
@@ -91,9 +91,9 @@ console.log("c",c);
 if(a <= b && a <= c) {
     console.log(a);
 }
-else if(b <= c && b <= a) {
-    console.log(b);
-}
+// else if(b <= c && b <= a) {
+//     console.log(b);
+// }
 // else {
 //     console.log(c);
 // }
