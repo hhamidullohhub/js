19647526941c3478e533e27e1f4ff9ca6c080054
MRX <131702289+hhamidullohgit@users.noreply.github.com>
2024-02-24 15:12:49 +0500

    add file

--- main.js
+++ main.js
@@ -77,39 +77,99 @@
 
 // console.log(x);
 // console.log(y);
-let a = Number(prompt("a"))//"4"
-let b = Number(prompt("b"))////"100"
-let c = Number(prompt("c"))// //"2"
-let d = Number(prompt("d"))// //"2"
-let e = Number(prompt("e"))// //"2"
-let f = Number(prompt("f"))// //"2"
-let g = Number(prompt("g"))// //"2"
-console.log("a",a);
-console.log("b",b);
-console.log("c",c);
+// let a = Number(prompt("biror bir son kiritng"))//"4"
+// let b = Number(prompt("biror bir son kiritng"))////"100"
+// let c = Number(prompt("biror bir son kiritng"))// //"2"
+// let d = Number(prompt("biror bir son kiritng"))// //"2"
+// let e = Number(prompt("biror bir son kiritng"))// //"2"
+// let f = Number(prompt("biror bir son kiritng"))// //"2"
+// let g = Number(prompt("biror bir son kiritng"))// //"2"
+// console.log("a",a);
+// console.log("b",b);
+// console.log("c",c);
+// console.log("c",d);
+// console.log("c",e);
+// console.log("c",f);
+// console.log("c",g);
 
-if(a <= b && a <= c) {
-    console.log(a);
+// if(a <= b && a <= c) {
+//     console.log(a);
+// }
+// else if(b <= c && b <= a) {
+//     console.log(b);
+// }
+// else {
+//     console.log(c);
+// }
+
+
+
+//6 ta raqamni eng kichigini yozish
+
+let number1 = Number(prompt("birinchi son"))
+let number2 = Number(prompt("ikkinchi son"))
+let number3 = Number(prompt("uchinchi son"))
+let number4 = Number(prompt("tortingchi son"))
+let number5 = Number(prompt("beshinchi son"))
+let number6 = Number(prompt("oltinchi son"))
+console.log("number1", number1);
+console.log("number2", number2);
+console.log("number3", number3);
+console.log("number4", number4);
+console.log("number5", number5);
+console.log("number6", number6);
+if (number1 <= number2 && number1 <= number3) {
+    console.log(number1)
 }
-else if(b <= c && b <= a) {
-    console.log(b);
+
+else if(number2 <= number3 && number2 <= number1) {
+    console.log(number2)
 }
+
 else {
-    console.log(c);
+    console.log(number3)
+}
+
+if (number4 <= number5 && number4 <= number6) {
+    console.log(number4)
+}
+
+else if (number5 <= number6 && number5 <= number4) {
+    console.log(number5)
+}
+
+else {
+    console.log(number6)
 }
 
 
 // 6 ta raqamnini eng kattasini yozish
 
+let num1 = Number(prompt("birinchi son"))
+let num2 = Number(prompt("ikkinchi son"))
+let num3 = Number(prompt("uchinchi son"))
+console.log("bu eng kattasini topb beardi")
+console.log("num1",num1)
+console.log("num2", num2)
+console.log("num3", num3)
+if (num1 >= num2 && num1 >= num3) {
+    console.log(num1)
+}
+else if (num2 >= num3 && num2 >= num1) {
+    console.log(num2)
+}
+
+else {
+    console.log(num3)
+}
 
 
 
 
+// let num4 = Number(prompt("biron son kiriting"))
+// let num5 = Number(prompt("biron son kiriting"))
+// let num6 = Number(prompt("biron son kiriting"))
 
-
-
-
-
-
-
-//6 ta raqamni eng kichigini yozish
+// console.log("num4", num4);
+// console.log("num5", num5);
+// console.log("num6", num6);
