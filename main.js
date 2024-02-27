444287a9241a7797a5b93df07285bb532d870255
MRX <131702289+hhamidullohhub@users.noreply.github.com>
2024-02-27 10:51:50 +0900

    i am add new file today

--- main.js
+++ main.js
@@ -224,44 +224,64 @@
 // console.log("number6", number6)
 
 
-let number1 = prompt("birimchi sonni kiriting").trim()
-let number2 = prompt("ikkinchi son").trim()
-let number3 = prompt("uchinchis son").trim()
-let number4 = prompt("tortincho son").trim()
-let number5 = prompt("beshinchi son").trim()
-let number6 = prompt("oltinchi son").trim()
-console.log("number1", number1)
-console.log("number2", number2)
-console.log("number3", number3)
-console.log("number4", number4)
-console.log("number5", number5)
-console.log("number6", number6)
-if (number1 && number2 && number3 && number4 && number5 && number6.length > 0) {
-    const num1 = Number(number1 && number2 && number3 && number4 && number5 && number6)
-    if (isNaN(num1)) {
-        console.log("raqam kiritmadingiz")
-    }else {
-        if (number1 <= number2 && number1 <= number3 && number1 <= number4 && number1 <= number5 && number1 <= number6) {
-            console.log(number1)
-        } else if (number2 <= number1 && number2 <= number3 && number2 <= number4 && number2 <= number5 && number3 <= number6) {
-            console.log(number2)
-        } else if (number3 <= number1 && number3 <= number2 && number3 <= number4 && number3 <= number5 && number3 <= number6) {
-            console.log(number3)
-        } else if (number4 <= number1 && number4 <= number2 && number4 <= number3 && number4 <= number5 && number4 <= number6) {
-            console.log(number4)
-        } else if (number5 <= number1 && number5 <= number2 && number5 <= number3 && number5 <= number4 && number5 <= number6) {
-            console.log(number5)
-        } else if (number6 <= number1 && number6 <= number2 && number6 <= number3 && number6 <= number4 && number6 <= number5) {
-            console.log(number6)
-        }
-    }
-}else {
-    console.log("raqam kiritmadingiz");
-}
 
 
 
 
+
+
+
+
+// let number1 = prompt("birimchi sonni kiriting").trim()
+// let number2 = prompt("ikkinchi son").trim()
+// let number3 = prompt("uchinchis son").trim()
+// let number4 = prompt("tortincho son").trim()
+// let number5 = prompt("beshinchi son").trim()
+// let number6 = prompt("oltinchi son").trim()
+// console.log("number1", number1)
+// console.log("number2", number2)
+// console.log("number3", number3)
+// console.log("number4", number4)
+// console.log("number5", number5)
+// console.log("number6", number6)
+// if (number1 && number2 && number3 && number4 && number5 && number6.length > 0) {
+//     const num1 = Number(number1 && number2 && number3 && number4 && number5 && number6)
+//     if (isNaN(num1)) {
+//         console.log("raqam kiritmadingiz")
+//     }else {
+//         if (number1 <= number2 && number1 <= number3 && number1 <= number4 && number1 <= number5 && number1 <= number6) {
+//             console.log(number1)
+//         } else if (number2 <= number1 && number2 <= number3 && number2 <= number4 && number2 <= number5 && number3 <= number6) {
+//             console.log(number2)
+//         } else if (number3 <= number1 && number3 <= number2 && number3 <= number4 && number3 <= number5 && number3 <= number6) {
+//             console.log(number3)
+//         } else if (number4 <= number1 && number4 <= number2 && number4 <= number3 && number4 <= number5 && number4 <= number6) {
+//             console.log(number4)
+//         } else if (number5 <= number1 && number5 <= number2 && number5 <= number3 && number5 <= number4 && number5 <= number6) {
+//             console.log(number5)
+//         } else if (number6 <= number1 && number6 <= number2 && number6 <= number3 && number6 <= number4 && number6 <= number5) {
+//             console.log(number6)
+//         }
+//     }
+// }else {
+//     console.log("raqam kiritmadingiz");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// biz kiritgan qiymatlar orasida neshtasidir raqam bolsa osha raqamlar ortasida taqqoslash bolishi kerakak qaysilaridir  xarf ili belgi bosa osha xarfli console.log()ga chqioshi kerak
 
 
 // if (numTrim.length > 0) {
@@ -284,20 +304,40 @@ if (number1 && number2 && number3 && number4 && number5 && number6.length > 0) {
 //     console.console.log();
 // }
 
+// let num1 = (prompt ("bironta son yozing")).trim()
+// if (num1.length > 0) {
+//     console.log(num1)
+// }else {
+//     console.log("hech narsa kiritmadingiz")
+// }
 
 
+// let yigirma = prompt("iltimos 20 dan kichik son kiriting").trim()
+// if (yigirma.length <= 0) {
+//     console.log("siz qiymat kiritmadingiz iltimos qiymat kiriting")
+// }else if (Number (yigirma > 20)) {
+//     console.log("siz kiritgan son 20 dan katta")
+// }else if (Number (yigirma < 20)) {
+//     console.log("RAXMAT")
+// }else if (Number (yigirma == 20)) {
+//     console.log("siz kiritgan son 20 ga teng")
+// }else  {
+//     console.log("iltimos raqam kiritng")
+// }
 
 
+let x = 555
+let y = 492
+let d = x-y
+console.log(d)
 
-
-
-
-
-
-
-
-
-
-// biz kiritgan qiymatlar orasida neshtasidir raqam bolsa osha raqamlar ortasida taqqoslash bolishi kerakak qaysilaridir  xarf ili belgi bosa osha xarfli console.log()ga chqioshi kerak
-
-
+// let num2 = (prompt ("bironta son yozing")).trim()
+// let num3 = (prompt ("bironta son yozing")).trim()
+// let num4 = (prompt ("bironta son yozing")).trim()
+// let num5 = (prompt ("bironta son yozing")).trim()
+// let num6 = (prompt ("bironta son yozing")).trim()
+// console.log(num2)
+// console.lof(num3)
+// console.log(num4)
+// console.log(num5)
+// console.log(num6)
